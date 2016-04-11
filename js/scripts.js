98135bf79e212cb228524e9c7ff351e2eb5af3c3
Blake Bless and DJ Roychoudhury <dj@>
2016-04-11 11:53:46 -0700

    initial files added.

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+// business
+  var arrayWords = ["one","two","three","four","five"]
+  //time
+  //
+
+
+
+//user
+$(document).ready(function(){
+    // function displayWord
+  $("form").submit(function(event){
+    event.preventDefault();
+    var userInput = $("input#playerInput").val();
+
+
+    // check accuracy of wordDisplay and wordInput (return true or false)
+    // if true output new word (displayWord) and reset form
+    // if false reset form
+  });
+})
