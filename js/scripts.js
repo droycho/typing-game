37492d2306434b8541a790927dad287c5785f451
Luca Quatela and Ryan Carlile <luca@>
2016-04-11 16:33:45 -0700

    add timers

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 // business
-  var arrayWords = ["one","two","three","four","five"]
+  var arrayWords = ["one","two","three","four","five","six","seven","eight","nine","ten" ]
   //time
   //
 
@@ -7,8 +7,36 @@
 
 
 
+
+
+
+// var timer = function(time){ // timer code line //
+//   setTimeout(function(){ alert("Game Over"); }, time) // timer code line //
+// }
+
+var timer = function(time){ // timer code line //
+  var timeinterval = setInterval(function(){ // timer code line //
+    if (time <= 0) { // timer code line //
+      $("#timer").text(""); // timer code line //
+      alert("Game Over"); // timer code line //
+      console.log("we are before clear interval"); // timer code line //
+      clearInterval(timeinterval); // timer code line //
+    } else { // timer code line //
+      $("#timer").text(time); // timer code line //
+      time = time - 1; // timer code line //
+    } // timer code line //
+  } , 1000); // timer code line //
+} // timer code line //
+
+
 //user
+
+
 $(document).ready(function(){
+  $("#playButton").click(function(){ // timer code line //
+    timer(10); // timer code line //
+  }); // timer code line //
+
   var num = 0;
   var nextWord = (function(){
     num ++;
