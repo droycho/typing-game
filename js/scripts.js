bae3207960a379137834398c4de3d97c146076d2
Blake Bless and DJ Roychoudhury <dj@>
2016-04-11 16:00:19 -0700

    added levels with different words.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
 // business
-  var arrayWords = ["one","two","three","four","five"]
-  //time
-  //
+var level1 = ["one","two","three","four","five"];
+var level2 = ["butter","cable","jingle","happy","water"];
+var level3 = ["beautiful","gasoline","potato","yesterday","magazine"];
+var level4 = ["photography","questionable","fundamental","caterpillar","alligator"];
+
+var levelArrays = [level1,level2,level3,level4];
+
+// var arrayWords = ["one","two","three","four","five"];
+//time
+
+
 
 
 
@@ -9,26 +17,30 @@
 
 //user
 $(document).ready(function(){
-  var num = 0;
-  var nextWord = (function(){
-    num ++;
-    $("#arrayTarget").text(arrayWords[num]);
-  })
 
-  $("#arrayTarget").text(arrayWords[0]);
-  // nextWord();
-  // console.log(nextWord());
+  var arrayNum = 0;
+  var wordNum = 0;
 
-    $("form").submit(function(event){
-      event.preventDefault();
-      var userInput = $("input#playerInput").val();
-      if (arrayWords[num] === userInput){
-      nextWord();
-      $("input#playerInput").val("");
+  var nextWord = (function(){ ///switches to next word in the array that it's currently in.
+    wordNum ++;
+  $("#arrayTarget").text(levelArrays[arrayNum][wordNum]);
+  });
+
+  var nextArray = (function(){ ///switches to next array in levelArrays.
+    arrayNum ++;
+  $("#arrayTarget").text(levelArrays[arrayNum][wordNum]);
+  });
+
+  $("#arrayTarget").text(levelArrays[0][0]);
+
+  $("form").submit(function(event){
+    event.preventDefault();
+    var userInput = $("input#playerInput").val();
+    if (levelArrays[arrayNum][wordNum] === userInput){
+    nextWord();
+    $("input#playerInput").val("");
     } else {
-      $("input#playerInput").val("");
+    $("input#playerInput").val("");
     }
-
-    // if false reset form
   });
 });
